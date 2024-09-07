3aa5f4f371066f2d3bc2ab999f4ff2a483087620
B5024 <149315654+B5024@users.noreply.github.com>
2024-09-07 20:04:30 +0800

    Add files via upload

--- src/main/java/com/example/fightthelandlord/gameWindow.java
+++ src/main/java/com/example/fightthelandlord/gameWindow.java
@@ -1,6 +1,7 @@
 package com.example.fightthelandlord;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,11 +10,22 @@ import com.example.fightthelandlord.Controllers.gameWindowController;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  *       gameWindow仅用于前端自己调试，后端所需仅看标注段落
  */
 public class gameWindow extends Application {
+
+    ArrayList<Card> HandCards = new ArrayList<>();
+    ArrayList<Card> BottomCards = new ArrayList<>();
+
+    Scene scene;
+    gameWindowController controller;
+
+    private final Object lock = new Object();
 
     public static void main(String[] args) {
         launch(args);
@@ -29,21 +41,131 @@ public class gameWindow extends Application {
         //   加载 FXML 文件并返回根节点（Parent）。
         Parent root = fxmlLoader.load();
         //   通过 fxmlLoader.getController() 获取 GameWindowController 的实例。
-        gameWindowController controller = fxmlLoader.getController();
-        Scene scene = new Scene(root, 1200, 800);
+        controller = fxmlLoader.getController();
+        scene = new Scene(root, 1200, 800);
 
         /* ******************************************* */
 
         /* 用于固定窗口大小，设置标题  */
         primaryStage.setTitle("gameWindow");
         primaryStage.setScene(scene);
-        primaryStage.initStyle(StageStyle.UTILITY); // 这种样式可以去掉大部分窗口装饰
+//        primaryStage.initStyle(StageStyle.UTILITY); // 这种样式可以去掉大部分窗口装饰
         primaryStage.setFullScreen(false);
         primaryStage.setResizable(false);
 
+        //  模拟发牌
+        testDealCard();
+        controller.setHandCard(HandCards);
+        controller.setBottomCard(BottomCards);
 
         primaryStage.show();//  窗口输出
 
+        //  额外新线程用于后端逻辑处理，与进行UI操作的JavaFX应用程序线程区分
+        new Thread(() -> {
+             test();
+        }).start();
     }
 
+
+    void test(){
+        /*
+         ************模拟进程******************
+         * 因UI操作都需在JavaFX应用程序线程中进行
+         * 所有UI操作都需写在Platform.runLater(() -> /具体操作/ )
+         * 用以将UI操作转到JavaFX应用程序线程
+         */
+        System.out.println("模拟开始");
+        // 模拟抢地主
+        Platform.runLater(() -> controller.setOtherPoint("l1"));
+        // 玩家回合开始
+        Platform.runLater(() -> controller.OnTurn());
+        // 等待isPlayed信号改变
+        while (!controller.isPlayed){
+            try{
+                Thread.sleep(10);
+            }catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+        }
+        // 任务完成后重置状态
+        controller.isPlayed = false;
+        // 回合结束，按钮禁用
+        Platform.runLater(() -> controller.setAllButtonDisable());
+
+        System.out.println("玩家抢点："+controller.Point);
+        Platform.runLater(() -> controller.setOtherPoint("r0"));
+        // 获得地主
+        Platform.runLater(() -> {
+            controller.addBottomCard();
+            controller.turnButton();
+            // 刷新牌桌
+            controller.refresh();
+        });
+        // 开始新回合
+        System.out.println("Begin");
+        Platform.runLater(() -> controller.OnTurn());
+        // 等待isPlayed信号改变
+        while (!controller.isPlayed){
+            try{
+                Thread.sleep(10);
+            }catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+        }
+        ArrayList<Card> cards = controller.getPlayedCards();
+        Deck deck = controller.getDeck();
+        System.out.println();
+        System.out.println(deck.getSize());
+        Platform.runLater(() -> {
+            controller.setOtherPlayedCard('r',cards);
+            controller.setOtherPlayedCard('l',cards);
+            controller.setDeck(deck);
+        });
+        System.out.println("again");
+        Platform.runLater(() -> controller.OnTurn());
+
+    }
+
+    void testDealCard(){
+        ArrayList<Card> deck = new ArrayList<>();
+        //创建一副牌,0表示3，13~小王，14~大王
+        int[] sizes = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        //牌的花色
+        int[] suits = {0, 1, 2, 3};
+
+        for (int size : sizes) {
+            for (int suit : suits) {
+                deck.add(new Card(size, suit));
+            }
+        }
+        //添加大小王
+        deck.add(new Card(13, 0));
+        deck.add(new Card(14, 0));
+        //洗牌
+        Collections.shuffle(deck);
+
+        //分发手牌
+        for (int i = 0; i < 51; i += 3) {
+            HandCards.add(deck.get(i));
+        }
+
+        //剩余三个地主牌
+        List<Card> subList = deck.subList(51, 54); // 获取子列表
+        BottomCards = new ArrayList<>(subList);
+
+        //洗牌
+        cardSorted(HandCards);
+
+    }
+
+    void cardSorted(ArrayList<Card> deck){
+        deck.sort((p1, p2) -> {
+            int sizeComparison = Integer.compare(p1.getSize(), p2.getSize());
+            if (sizeComparison != 0) {
+                return sizeComparison;
+            } else {
+                return Integer.compare(p1.getSuit(), p2.getSuit());
+            }
+        });
+    }
 }
